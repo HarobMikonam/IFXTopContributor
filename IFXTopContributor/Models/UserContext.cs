77b5235e428cb7923172f8fe21f5b845f4b72151
HarobMikonam <mattfaghiri@gmail.com>
2024-07-04 20:05:36 +0300

    Removal of enums that were done through scaffold dbcontext

--- IFXTopContributor/Models/UserContext.cs
+++ IFXTopContributor/Models/UserContext.cs
@@ -1,77 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
-namespace IFXTopContributor.Models;
-
-public partial class UserContext : DbContext
+namespace IFXTopContributor.Models
 {
-    public UserContext()
+    public partial class UserContext : DbContext
     {
-    }
-
-    public UserContext(DbContextOptions<UserContext> options)
-        : base(options)
-    {
-    }
-
-    public virtual DbSet<User> Users { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder
-            .HasPostgresEnum("auth", "aal_level", new[] { "aal1", "aal2", "aal3" })
-            .HasPostgresEnum("auth", "code_challenge_method", new[] { "s256", "plain" })
-            .HasPostgresEnum("auth", "factor_status", new[] { "unverified", "verified" })
-            .HasPostgresEnum("auth", "factor_type", new[] { "totp", "webauthn" })
-            .HasPostgresEnum("auth", "one_time_token_type", new[] { "confirmation_token", "reauthentication_token", "recovery_token", "email_change_token_new", "email_change_token_current", "phone_change_token" })
-            .HasPostgresEnum("pgsodium", "key_status", new[] { "default", "valid", "invalid", "expired" })
-            .HasPostgresEnum("pgsodium", "key_type", new[] { "aead-ietf", "aead-det", "hmacsha512", "hmacsha256", "auth", "shorthash", "generichash", "kdf", "secretbox", "secretstream", "stream_xchacha20" })
-            .HasPostgresEnum("realtime", "action", new[] { "INSERT", "UPDATE", "DELETE", "TRUNCATE", "ERROR" })
-            .HasPostgresEnum("realtime", "equality_op", new[] { "eq", "neq", "lt", "lte", "gt", "gte", "in" })
-            .HasPostgresExtension("extensions", "pg_stat_statements")
-            .HasPostgresExtension("extensions", "pgcrypto")
-            .HasPostgresExtension("extensions", "pgjwt")
-            .HasPostgresExtension("extensions", "uuid-ossp")
-            .HasPostgresExtension("graphql", "pg_graphql")
-            .HasPostgresExtension("pgsodium", "pgsodium")
-            .HasPostgresExtension("vault", "supabase_vault");
-
-        modelBuilder.Entity<User>(entity =>
+        public UserContext()
         {
-            entity.HasKey(e => e.Id).HasName("users_pkey");
+        }
 
-            entity.ToTable("users");
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
 
-            entity.HasIndex(e => e.Email, "users_email_key").IsUnique();
+        public virtual DbSet<User> Users { get; set; }
 
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.Createdat)
-                .HasDefaultValueSql("CURRENT_TIMESTAMP")
-                .HasColumnType("timestamp without time zone")
-                .HasColumnName("createdat");
-            entity.Property(e => e.Email)
-                .HasMaxLength(100)
-                .HasColumnName("email");
-            entity.Property(e => e.Firstname)
-                .HasMaxLength(100)
-                .HasColumnName("firstname");
-            entity.Property(e => e.Lastname)
-                .HasMaxLength(100)
-                .HasColumnName("lastname");
-            entity.Property(e => e.Role)
-                .HasMaxLength(50)
-                .HasColumnName("role");
-            entity.Property(e => e.Updatedat)
-                .HasDefaultValueSql("CURRENT_TIMESTAMP")
-                .HasColumnType("timestamp without time zone")
-                .HasColumnName("updatedat");
-        });
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("users_pkey");
 
-        modelBuilder.HasSequence<int>("seq_schema_version", "graphql").IsCyclic();
+                entity.ToTable("users");
 
-        OnModelCreatingPartial(modelBuilder);
+                entity.HasIndex(e => e.Email, "users_email_key").IsUnique();
+
+                entity.Property(e => e.Id).HasColumnName("id");
+                entity.Property(e => e.Createdat)
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("createdat");
+                entity.Property(e => e.Email)
+                    .HasMaxLength(100)
+                    .HasColumnName("email");
+                entity.Property(e => e.Firstname)
+                    .HasMaxLength(100)
+                    .HasColumnName("firstname");
+                entity.Property(e => e.Lastname)
+                    .HasMaxLength(100)
+                    .HasColumnName("lastname");
+                entity.Property(e => e.Role)
+                    .HasMaxLength(50)
+                    .HasColumnName("role");
+                entity.Property(e => e.Updatedat)
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                    .HasColumnType("timestamp without time zone")
+                    .HasColumnName("updatedat");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
