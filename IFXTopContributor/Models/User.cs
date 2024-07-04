00527c01b6db8b4dee6e81919693201c7f058159
HarobMikonam <mattfaghiri@gmail.com>
2024-07-04 20:03:59 +0300

    Creation of Models using database first approach

---
+++ IFXTopContributor/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IFXTopContributor.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Firstname { get; set; } = string.Empty;
+
+    public string Lastname { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Role { get; set; } = string.Empty;
+
+    public DateTime Createdat { get; set; }
+
+    public DateTime Updatedat { get; set; } = DateTime.UtcNow;
+}
