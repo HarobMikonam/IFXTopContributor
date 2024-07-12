3115d24b94c3dfcd94ffbe2237e49061770c838c
HarobMikonam <mattfaghiri@gmail.com>
2024-07-12 17:21:24 +0300

    Added the implementation fo querying by roles using LINQ.

--- IFXTopContributor/Models/User.cs
+++ IFXTopContributor/Models/User.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace IFXTopContributor.Models;
 
 public partial class User
 {
+    [Required]
     public int Id { get; set; }
-
+    [Required]
     public string? Firstname { get; set; } = string.Empty;
-
+    [Required]
     public string? Lastname { get; set; } = string.Empty;
-
+    [Required]
     public string? Email { get; set; } = string.Empty;
-
+    [Required]
     public string? Role { get; set; } = string.Empty;
 
     public DateTime? Createdat { get; set; }
